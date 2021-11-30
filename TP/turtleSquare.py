95826614b286a3e9f4fcf1f9cc7f6d0b2ed6e453
Thomas120501 <93184722+Thomas120501@users.noreply.github.com>
2021-11-30 09:57:22 +0100

    Add files via upload

---
+++ TP/turtleSquare.py
@@ -0,0 +1,19 @@
+import turtle
+maTortue = turtle.Turtle()
+
+maTortue.speed(1)
+
+maTortue.forward(100)
+maTortue.left(90)
+
+maTortue.forward(100)
+maTortue.left(90)
+
+maTortue.forward(100)
+maTortue.left(90)
+
+maTortue.forward(100)
+
+input()
+
+
